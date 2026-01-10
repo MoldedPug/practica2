b5fba54fafd9848a36d34ab013a16f26da4178f2
MoldedPug <oriolbolta@gmail.com>
2026-01-10 19:57:39 +0100

    Error 1: he pujat el contingut de tauler.h a tauler.c. Intercanvi.

---
+++ tauler.h
@@ -0,0 +1,15 @@
+#ifndef TAULER_H /* guard per evitar duplicats, així el codi del .h només entra la primera vegada */
+#define TAULER_H
+
+// Les constants del joc
+#define MIDA 10
+#define AIGUA 0
+#define VAIXELL 1
+#define AIGUA_TOCADA 2
+
+// Les funcions del tauler
+void inicialitzar_tauler(int tauler[MIDA][MIDA]);
+void mostrar_tauler(int tauler[MIDA][MIDA], int mostrar_vaixells);
+void copiar_tauler(int origen[MIDA][MIDA], int desti[MIDA][MIDA]);
+
+#endif
