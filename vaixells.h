de19a6d173e5dad989ac6f2d7c35148e31c8e3f5
MoldedPug <oriolbolta@gmail.com>
2026-01-11 19:39:17 +0100

    obtenir_configuracio_flota i es_posicio_valida

--- vaixells.h
+++ vaixells.h
@@ -23,4 +23,12 @@ typedef struct {
     int submarins;
 } Flota;
 
+// Funcions de vaixells
+/*
+    obtenir_configuracio_flota : Per saber quin tipus de flota tindrem passem el nom 
+    1: L'Armada Vencible,  2: Els Brivalls,  3: La Flotilla.
+*/ 
+void obtenir_configuracio_flota(int tipus, Flota *flota);
+int es_posicio_valida(int tauler[MIDA][MIDA], int fila, int col, int mida, int orientacio);
+// Millor sense dieresi (per compatibilitat)
 #endif
